74cbf1f571b35c7cd91cbc10a56c12f4fd1f2eee
Kumar Harshit <108181883+Kumar-Harshit223@users.noreply.github.com>
2023-04-15 19:14:06 +0530

    Updated Question05

--- 2nd year/4th Sem/Assignment2A/Question05.java
+++ 2nd year/4th Sem/Assignment2A/Question05.java
@@ -1,24 +1,47 @@
 import java.util.*;
 public class Question05 {
 
+	static class Tree{
+		int data;
+		Tree right;
+		Tree left;
+		
+		Tree(int data){
+			this.data=data;
+			this.right=null;
+			this.left=null;
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {1,2,3,4,5,0,6,7,0,0,0};
-		bfsTraversal(arr, 1);
+		
+		Tree root=new Tree(1);
+		root.left=new Tree(2);
+		root.right=new Tree(3);
+		
+		root.left.left=new Tree(4);
+		root.right.right=new Tree(5);
+		
+		root.left.left.left=new Tree(6);
+		root.right.right.right=new Tree(7);
+		
+		bfsTraversal(root);
+		
 	}
-	public static void bfsTraversal(int[]arr, int root) {
-		boolean[] visit=new boolean[arr.length];
-		ArrayDeque<Integer> que=new ArrayDeque<Integer>();
-		que.add(root);
-		visit[0]=true;
-		while(!que.isEmpty()) {
-			int res=que.remove();
-			System.out.print(res+" ");
-			for(int i=0;i<arr.length;i++) {
-				if(arr[i]!=0&&!visit[i]) {
-					que.add(i);
-					visit[i]=true;
-				}
+	
+	public static void bfsTraversal(Tree root) {
+		if(root == null)
+			return;
+		ArrayDeque<Tree> bfs= new ArrayDeque<Tree>();
+		bfs.add(root);
+		while(!bfs.isEmpty()) {
+			Tree curr=bfs.remove();
+			System.out.print(curr.data+" ");
+			if(curr.left!=null) {
+				bfs.add(curr.left);
+			}
+			if(curr.right!=null) {
+				bfs.add(curr.right);
 			}
 		}
 	}
